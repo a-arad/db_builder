faacb0d742827ff184e8a74330b05d021ead9e3c
aradmschool <arad.n@masterschool.com>
2023-12-30 15:33:45 -0500

    remove read file update init

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,3 +3,4 @@
 from .parse_schema import parse_schema
 from .generate_table_command import generate_table_command
 from .read_csv_files import read_csv_files
+from .execute_with_psql import execute_with_psql
---
+++ utils/execute_with_psql.py
@@ -0,0 +1,7 @@
+import subprocess
+
+def execute_with_psql(sql_file, db_connection_string):
+    '''runs the sql file '''
+    psql_command = f"psql {db_connection_string} -f {sql_file}"
+    subprocess.run(psql_command, shell=True, check=True)
+
