892bc6e08de798f8d65f9bd73c6fbdaefc470055
aradmschool <arad.n@masterschool.com>
2023-12-30 15:17:34 -0500

    psql

--- utils/generate_sql_commands.py
+++ utils/generate_sql_commands.py
@@ -1,9 +1,17 @@
+import os
 from utils.generate_table_command import generate_table_command
 
-def generate_sql_commands(schema):
+def generate_sql_commands(schema,folder_path):
     """generates SQL commands for all tables in the schema."""
-    commands = []
+    def create_copy_command(table_name, file_path):
+        '''adds psql copy command'''
+        return f"\copy {table_name} FROM '{file_path}' DELIMITER ',' CSV HEADER NULL AS ''"
+    commands = ""
     for table, columns in schema.items():
-        command = generate_table_command(table, columns)
-        commands.append(command)
-    return commands
+        commands += generate_table_command(table, columns) + ";\n"
+        # assumes file names match table names
+        csv_file_path = os.path.join(folder_path, f"{table}.csv")
+        commands += create_copy_command(table, csv_file_path) + "\n"
+    sql_script_path = f'create_{os.path.basename(folder_path)}.sql'
+    with open(sql_script_path, 'w') as f:
+      f.write(commands)
