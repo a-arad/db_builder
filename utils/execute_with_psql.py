5abe63da8fb587e9bca6cbd69d6d7866932845cd
aradmschool <arad.n@masterschool.com>
2024-02-21 18:43:24 -0500

    vibestream and from main execute

--- utils/execute_with_psql.py
+++ utils/execute_with_psql.py
@@ -5,3 +5,7 @@ def execute_with_psql(sql_file, db_connection_string):
     psql_command = f"psql {db_connection_string} -f {sql_file}"
     subprocess.run(psql_command, shell=True, check=True)
 
+if __name__ == "__main__":
+
+    import sys
+    execute_with_psql(sql_file=sys.argv[1],db_connection_string=sys.argv[1])
